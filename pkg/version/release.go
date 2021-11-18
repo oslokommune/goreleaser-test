97b74b04230e9105a8fc586418640631dab7fc75
Yngvar Kristiansen <562343+yngvark@users.noreply.github.com>
2021-11-18 15:52:02 +0100

    add version

---
+++ pkg/version/release.go
@@ -0,0 +1,14 @@
+// Package version exists
+package version
+
+// Version of okctl
+// nolint: gochecknoglobals
+var Version = "dev"
+
+// ShortCommit hash
+// nolint: gochecknoglobals
+var ShortCommit = "unknown"
+
+// BuildDate of the release
+// nolint: gochecknoglobals
+var BuildDate = "unknown"
